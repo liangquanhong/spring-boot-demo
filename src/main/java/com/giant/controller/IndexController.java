c9ece03b82c1e2a07fb1cfb9583af81d5497f176
liangquanhong <l>
2017-01-11 15:30:43 +0800

    提交代码

---
+++ src/main/java/com/giant/controller/IndexController.java
@@ -0,0 +1,50 @@
+package com.giant.controller;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.giant.bean.User;
+
+@RestController
+@RequestMapping(value = "/index")
+public class IndexController {
+
+	@Value(value = "${giantor.secret}")
+	private String secret;
+	
+	@Value(value = "${giantor.number}")
+	private int number;
+	
+	@RequestMapping
+	public String index() {
+		return "hello world";
+	}
+
+	// @RequestParam 简单类型的绑定，可以出来get和post
+	@RequestMapping(value = "/get")
+	public Map<String, Object> get(@RequestParam String name) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("title", "hello world");
+		map.put("name", name);
+		map.put("secret", secret);
+		map.put("number", number);
+		return map;
+	}
+	
+	// @PathVariable 获得请求url中的动态参数
+	@RequestMapping(value = "/get/{id}/{name}")
+	public User getUser(@PathVariable int id, @PathVariable String name) {
+		User user = new User();
+		user.setId(id);
+		user.setName(name);
+		user.setDate(new Date());
+		return user;
+	}
+}
---
+++ src/main/java/com/giant/task/ScheduledTasks.java
@@ -0,0 +1,19 @@
+package com.giant.task;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    //@Scheduled(fixedRate = 5000)
+	@Scheduled(cron="*/5 * * * * *")
+    public void reportCurrentTime() {
+        System.out.println("当前时间：" + dateFormat.format(new Date()));
+    }
+}
