897b6ccb5d4dcaf56989a783abba124adae2a102
giantor <giantor@163.com>
2020-11-22 20:15:36 +0800

    test1

--- src/main/java/com/giant/SpringBootDemoApplication.java
+++ src/main/java/com/giant/SpringBootDemoApplication.java
@@ -11,5 +11,6 @@ public class SpringBootDemoApplication {
 	public static void main(String[] args) {
 		System.out.printle("Hello world!");
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+		System.out.printle("Good night!");
 	}
 }
