294ee50affa64cf8c847e9abbae1a910b1f90d58
giantor <giantor@163.com>
2020-11-22 20:21:43 +0800

    merge

--- src/main/java/com/giant/SpringBootDemoApplication.java
+++ src/main/java/com/giant/SpringBootDemoApplication.java
@@ -11,5 +11,6 @@ public class SpringBootDemoApplication {
 	public static void main(String[] args) {
 		System.out.printle("Hello world20201122!");
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+		System.out.printle("Good night!");
 	}
 }
